af2479c6d51ae362673c08cc04d12390925a3011
Tank <johnson90.cm@gmail.com>
2021-12-08 11:39:07 -0700

    seed selectors

---
+++ assets/sql/seed.sql
@@ -0,0 +1,26 @@
+--  selectors
+SELECT
+    *
+FROM
+    employee;
+
+SELECT
+    role.id,
+    role.title,
+    role.salary
+FROM
+    role
+ORDER BY
+    role.id;
+
+SELECT
+    role.id,
+    role.title
+FROM
+    role
+ORDER BY
+    role.id;
+
+-- department selectors
+-- role selectors
+-- employee selectors
