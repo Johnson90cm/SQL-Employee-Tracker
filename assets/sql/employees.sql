9c4d35b3cc1922c6567e878d0447856031c27f54
Tank <johnson90.cm@gmail.com>
2021-12-08 11:21:37 -0700

    value tables

---
+++ assets/sql/employees.sql
@@ -0,0 +1,41 @@
+DROP DATABASE IF EXISTS employees;
+
+CREATE DATABASE employees;
+
+USE employees;
+
+CREATE TABLE employee ();
+
+CREATE TABLE department ();
+
+CREATE TABLE employee ();
+
+VALUES
+    ('Management'),
+    ('Chemistry'),
+    ('Software Engineering'),
+    ('Mechanical Engineering');
+
+VALUES
+    ('CEO', 1000000, 1),
+    ('Project Director', 750000, 1),
+    ('VP: Engineering', 500000, 2),
+    ('VP: Chemistry', 500000, 3),
+    ('Junior Software Engineer', 100000, 2),
+    ('Senior Software Engineer', 250000, 2),
+    ('Junior Mechanical Engineer', 100000, 2),
+    ('Senior Mechanical Engineer', 250000, 2),
+    ('Junior Chemist', 100000, 3),
+    ('Senior Chemist', 250000, 3);
+
+VALUES
+    ('Ronald', 'Firbank', 1, 1),
+    ('Virginia', 'Woolf', 1, 1),
+    ('Piers', 'Gaveston', 1, 0),
+    ('Charles', 'LeRoi', 2, 1),
+    ('Katherine', 'Mansfield', 2, 1),
+    ('Dora', 'Carrington', 3, 0),
+    ('Edward', 'Bellamy', 3, 0),
+    ('Montague', 'Summers', 3, 1),
+    ('Octavia', 'Butler', 3, 1),
+    ('Unica', 'Zurn', NULL, 1);
